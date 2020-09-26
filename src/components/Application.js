3321a99149cbe837aa8684b1b96ad4543297c41d
shruthig <shruthigangadharblr@email.com>
2020-09-26 08:09:06 -0700

    initial commit

---
+++ src/components/Application.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "components/Application.scss";
+
+export default function Application(props) {
+  return (
+    <main className="layout">
+      <section className="sidebar">
+      <img
+  className="sidebar--centered"
+  src="images/logo.png"
+  alt="Interview Scheduler"
+/>
+<hr className="sidebar__separator sidebar--centered" />
+<nav className="sidebar__menu"></nav>
+<img
+  className="sidebar__lhl sidebar--centered"
+  src="images/lhl.png"
+  alt="Lighthouse Labs"
+/>
+      </section>
+     
+      <section className="schedule">
+     
+      </section>
+    </main>
+  );
+}
